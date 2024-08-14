34ba384e8b9baf6b97061e0500b020a4601eec3b
Abhinash kumar <147434873+ABHINASHYA@users.noreply.github.com>
2024-08-14 16:29:45 +0530

    Time: 0 ms (100%), Space: 41.9 MB (69.97%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n); // high compexity---O(x^n)
+        //return Math.pow(x,n); // high compexity---O(x^n) //this is one line code
+       return find(x,(long) n);
+    }
+    double find(double x, long n){
+        if(n<0){
+            return 1.0/find(x, -n);
+        }
+        if(n==0){
+            return 1;
+        }
+        if(x==0){
+            return 0;
+        }
+        double ans=find(x, n/2);
+        if(n%2==0){
+            return ans*ans;
+        }
+        return x*ans*ans;
     }
 }
