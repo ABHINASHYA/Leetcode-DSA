673b475ebc1aa913cd2f0ab6ab4d69d13d6774c8
Abhinash kumar <147434873+ABHINASHYA@users.noreply.github.com>
2024-10-16 17:56:30 +0530

    Time: 356 ms (5.96%), Space: 46.6 MB (6.18%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,24 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length-1; i=i+2){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
+    public int singleNumber(int[] arr) {
+        // Arrays.sort(nums);
+        // for(int i=0; i<nums.length-1; i=i+2){
+        //     if(nums[i] != nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[nums.length-1];
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            int num = arr[i];
+            int count = 0;
+            for(int j=0; j<n; j++){
+                if(arr[j]== num){
+                    count++;
+                }
+                
             }
+            if(count == 1) return num;
         }
-        return nums[nums.length-1];
-    }
+        return -1;
+    }    //coplexity= nlogn*n
 }
