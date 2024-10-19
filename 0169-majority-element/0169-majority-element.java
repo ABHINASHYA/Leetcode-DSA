25195d7fe7abf0028c78bd743e0c70a22728e9a0
Abhinash kumar <147434873+ABHINASHYA@users.noreply.github.com>
2024-10-19 17:53:01 +0530

    Time: 13 ms (30.63%), Space: 49.5 MB (76.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -5,8 +5,8 @@
 //     }
 // }
 
-class Solution {
-    public int majorityElement(int[] nums) {
+/*class Solution {
+    public int majorityElement(int[] nums) {   //tc = O(n*n)
         int n = nums.length;
         for(int  i=0; i<n; i++ ){
             int count = 0;
@@ -15,6 +15,25 @@ class Solution {
                 count++;
             }
             if(count > n/2) return nums[i];
+        }
+        return -1;
+    }
+}*/
+
+class Solution {
+    public int majorityElement(int[] nums) {   //tc = O(n*n)
+        int n = nums.length;
+        
+        HashMap<Integer, Integer> mpp =new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            int value = mpp.getOrDefault(nums[i], 0);
+            mpp.put(nums[i], value + 1);
+        }
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() > (n / 2)) {
+                return it.getKey();
+            }
         }
         return -1;
     }
