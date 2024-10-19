366c3392293681432e50841b31176afa79a43c36
Abhinash kumar <147434873+ABHINASHYA@users.noreply.github.com>
2024-10-19 18:27:52 +0530

    Time: 2 ms (77.93%), Space: 52.8 MB (64.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -20,8 +20,8 @@
     }
 }*/
 
-class Solution {
-    public int majorityElement(int[] nums) {   //tc = O(n*n)
+/*class Solution {
+    public int majorityElement(int[] nums) {   //tc = O(2*n)
         int n = nums.length;
         
         HashMap<Integer, Integer> mpp =new HashMap<>();
@@ -35,6 +35,35 @@ class Solution {
                 return it.getKey();
             }
         }
+        return -1;
+    }
+}*/
+
+//optimal approch
+// O(N)
+
+class Solution {
+    public int majorityElement(int[] nums) {
+       int n = nums.length;
+        int count = 0; 
+        int element = 0; 
+
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                count = 1;
+                element = nums[i];
+            } else if (element == nums[i]) count++;
+            else count--;
+        }
+
+        //checking if the stored element
+        // is the majority element:
+        int count1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == element) count1++;
+        }
+
+        if (count1 > (n / 2)) return element;
         return -1;
     }
 }
