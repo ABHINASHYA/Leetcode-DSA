04cfe82e6448713782d6f94de2a68ac0b99efe90
Abhinash kumar <147434873+ABHINASHYA@users.noreply.github.com>
2024-10-26 14:49:07 +0530

    Time: 1446 ms (5.06%), Space: 53.2 MB (30.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -5,20 +5,20 @@
 //     }
 // }
 
-/*class Solution {
+class Solution {
     public int majorityElement(int[] nums) {   //tc = O(n*n)
         int n = nums.length;
-        for(int  i=0; i<n; i++ ){
-            int count = 0;
+        for(int i=0; i<n; i++){
+            int count =0;
             for(int j=0; j<n; j++){
                 if(nums[j] == nums[i])
                 count++;
             }
-            if(count > n/2) return nums[i];
+            if(count> n/2) return nums[i];
         }
         return -1;
     }
-}*/
+}
 
 /*class Solution {
     public int majorityElement(int[] nums) {   //tc = O(2*n)
@@ -42,28 +42,28 @@
 //optimal approch
 // O(N)
 
-class Solution {
-    public int majorityElement(int[] nums) {
-       int n = nums.length;
-        int count = 0; 
-        int element = 0; 
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//        int n = nums.length;
+//         int count = 0; 
+//         int element = 0; 
 
-        for (int i = 0; i < n; i++) {
-            if (count == 0) {
-                count = 1;
-                element = nums[i];
-            } else if (element == nums[i]) count++;
-            else count--;
-        }
+//         for (int i = 0; i < n; i++) {
+//             if (count == 0) {
+//                 count = 1;
+//                 element = nums[i];
+//             } else if (element == nums[i]) count++;
+//             else count--;
+//         }
 
-        //checking if the stored element
-        // is the majority element:
-        int count1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == element) count1++;
-        }
+//         //checking if the stored element
+//         // is the majority element:
+//         int count1 = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] == element) count1++;
+//         }
 
-        if (count1 > (n / 2)) return element;
-        return -1;
-    }
-}
+//         if (count1 > (n / 2)) return element;
+//         return -1;
+//     }
+// }
