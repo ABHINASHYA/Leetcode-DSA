fc87404f0e4d5e68a2186fc551c92bac76ac8479
Abhinash kumar <147434873+ABHINASHYA@users.noreply.github.com>
2024-11-18 23:29:07 +0530

    Time: 0 ms (100%), Space: 42.5 MB (33.58%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,7 +3,6 @@ class Solution {
         if(nums.length==1) return 0;
         if(nums[0] > nums[1]) return 0;
         if(nums[nums.length-1]>nums[nums.length-2]) return nums.length-1;
-           //these line code are indicate peak eliment are present in first  index or last index
 
         int low=1;
         int high=nums.length-2;
