8f7255fa30cfa57b3986762ab6cb39c1654c7ec3
Abhinash kumar <147434873+ABHINASHYA@users.noreply.github.com>
2025-11-11 15:15:30 +0530

    Time: 0 ms (100%), Space: 48.6 MB (5.6%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,29 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
-       /* for(int i=0; i<nums.length; i++){
-            if(nums[i]== target)return i;
-        }
-        return - 1;*/
-        int start = 0;
-        int end = nums.length-1;
+    //    /* for(int i=0; i<nums.length; i++){
+    //         if(nums[i]== target)return i;
+    //     }
+    //     return - 1;*/
+    //     int start = 0;
+    //     int end = nums.length-1;
 
-        while(start<=end){
-            int mid = (start+end)/2;
+    //     while(start<=end){
+    //         int mid = (start+end)/2;
 
-            if(nums[mid]==target){
-            return mid;
+    //         if(nums[mid]==target){
+    //         return mid;
 
-            }
-            if(nums[mid] < target){
-                start= mid+1;
-            }else{
-                end=mid-1;
-            }
-
-        
-        }
-        return - 1;
+    //         }
+    //         if(nums[mid] < target){
+    //             start= mid+1;
+    //         }else{
+    //             end=mid-1;
+    //         }
 
         
+    //     }
+    //     return - 1;
+  int start=0;
+  int end=nums.length-1;
+  while(start<=end){
+    int mid =(start+end)/2;
+    if(target==nums[mid]){
+        return mid;
+    }
+    if(target>nums[mid]){
+        start=mid+1;
+    }else{
+        end=mid-1;
+    }
+  }
+      return -1;  
     }
 }
