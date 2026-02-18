224b3ee2ef9a5333b362c80cee4273ba66648445
Abhinash kumar <147434873+ABHINASHYA@users.noreply.github.com>
2026-02-19 02:30:34 +0530

    Time: 0 ms (100%), Space: 42.6 MB (16.14%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int currBit = n%2;   // O(nlogn)
+        n /= 2;
+
+        while(n>0){
+            if(currBit == n%2){
+                return false;
+            }
+            currBit = n%2;
+            n /= 2;
+        }
+        return true;
+    }
+}
